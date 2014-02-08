58849a343398232cd499066d35a307fbe599906d
Alejandro Morales <morales.s.alejandro@gmail.com>
2014-02-08 22:42:40 +0100

    Dimensions of the canopy are update dynamically, but respiration, full allocation and growth not included yet

--- growth_module.f90
+++ growth_module.f90
@@ -1,19 +1,19 @@
 Module growth_module
 Implicit None
 INTEGER :: IOERROR
-double precision :: RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, RF_leaf, RF_shoots, RF_stem, RF_froots, RF_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, CC_leaf, CC_shoots, CC_stem, CC_froots, CC_croots, sla, LAD, Q10, cp, k, RUEg, Hmax, Kroots, CCfr, CCoil, RFfr, RFoil, PGoil, PGfr, Kf, PVfr, PVoil, FractionReproductive, OneLeaf,  PVres, RFres, CCres, PCfr, PCoil, Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, V, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T
+double precision :: RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, RF_leaf, RF_shoots, RF_stem, RF_froots, RF_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, CC_leaf, CC_shoots, CC_stem, CC_froots, CC_croots, LAD, Q10, cp, k, RUEg, Hmax, Kroots, CCfr, CCoil, RFfr, RFoil, PGoil, PGfr, Kf, PVfr, PVoil, FractionReproductive, OneLeaf,  PVres, RFres, CCres, PCfr, PCoil, d_alley, d_row, Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Volume, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, specific_leaf_area
 integer :: OptPrun, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, Adult, DOYsenescence1, DOYsenescence2, Age
 character(LEN = 10) :: DensOpt, OptFruit
 
-Namelist /growth_parameters/ RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, RF_leaf, RF_shoots, RF_stem, RF_froots, RF_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, CC_leaf, CC_shoots, CC_stem, CC_froots, CC_croots, sla, LAD, Q10, cp, k, RUEg, Hmax, DensOpt, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, Kroots, CCfr, CCoil, RFfr, RFoil, PGoil, PGfr, Kf, PVfr, PVoil, Adult, FractionReproductive, OneLeaf, DOYsenescence1, DOYsenescence2, PVres, RFres, CCres, PCfr, PCoil, OptFruit, OptPrun
+Namelist /growth_parameters/ RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, RF_leaf, RF_shoots, RF_stem, RF_froots, RF_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, CC_leaf, CC_shoots, CC_stem, CC_froots, CC_croots, LAD, Q10, cp, k, RUEg, Hmax, DensOpt, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, Kroots, CCfr, CCoil, RFfr, RFoil, PGoil, PGfr, Kf, PVfr, PVoil, Adult, FractionReproductive, OneLeaf, DOYsenescence1, DOYsenescence2, PVres, RFres, CCres, PCfr, PCoil, OptFruit, OptPrun, d_alley, d_row, specific_leaf_area
 
-Namelist /growth_initial/ Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, V, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, Age
+Namelist /growth_initial/ Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Volume, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, Age
 
 contains
 
 
 subroutine read_growth_inputs
-USE maestcom, only: ugrowth, out_growth, iwarn
+USE maestcom, only: ugrowth, out_growth, ifatal
 Implicit none
 
 ! Open the input file called 'Growth.dat'   
@@ -21,32 +21,39 @@ Implicit none
 ! Trigger a warning if the file does not exist (model runs with default values)
     IF(IOERROR.NE.0) THEN
         close(ugrowth)
-        CALL SUBERROR('ERROR: Growth.dat DOES NOT EXIST', IWARN,IOERROR)
+        CALL SUBERROR('ERROR: Growth.dat DOES NOT EXIST', IFATAL,IOERROR)
     ENDIF
 ! Read the parameters
     READ(UGROWTH, nml = growth_parameters, IOSTAT = IOERROR)
 ! Trigger a warning if there is a problem reading the namelist
     IF(IOERROR.NE.0) THEN
         close(ugrowth)
-        CALL SUBERROR('ERROR: namelist growth_parameters was not red properly', IWARN,IOERROR)
-    ENDIF       
+        CALL SUBERROR('ERROR: namelist growth_parameters was not red properly', IFATAL,IOERROR)
+    ENDIF      
+    rewind(UGROWTH) 
 ! Read the initial values for the state and auxilliary variables
 	Read(ugrowth, nml = growth_initial, IOSTAT = IOERROR)    
 ! Trigger a warning if there is a problem reading the namelist
     IF(IOERROR.NE.0) THEN
         close(ugrowth)
-        CALL SUBERROR('ERROR: namelist growth_initial was not red properly', IWARN,IOERROR)
+        CALL SUBERROR('ERROR: namelist growth_initial was not red properly', IFATAL,IOERROR)
     ENDIF    
     close(ugrowth)
 
 ! Open the output file called 'out_growth.dat'   
-    OPEN(out_growth, FILE= 'out_growth.dat', STATUS = 'REPLACE', IOSTAT = IOERROR)
+    OPEN(out_growth, FILE= 'out_growth.dat', STATUS = 'OLD', IOSTAT = IOERROR,ACTION = 'WRITE')
     IF(IOERROR.NE.0) THEN
     	close(out_growth)
-        CALL SUBERROR('ERROR: out_growth.dat does not exist', IWARN,IOERROR)
+        CALL SUBERROR('ERROR: out_growth.dat does not exist', IFATAL,IOERROR)
     ENDIF 
-    Write(out_growth, '(14A15)') 'Year','DOY','Leaf','Shoots','Stem','Fine_roots','Coarse_roots','Fruits','Reserves','Volume','LAI','Height','Radius_x','Radius_y'
+    Write(out_growth, '(12(A10,3x))') 'Leaf','Shoots','Stem','Fine_roots','Coarse_roots','Fruits','Reserves','Volume','LAI','Height','Radius_x','Radius_y'
 end subroutine read_growth_inputs
+
+subroutine write_growth_outputs
+USE maestcom, only: out_growth
+Implicit None
+	Write(out_growth, '(12(ES12.6,3x))') Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Reserves, Volume, LAI, H, Rx, Ry
+end subroutine 
 
 subroutine growth_finalize
 Use maestcom, only: out_growth
--- maespa_growth.f90
+++ maespa_growth.f90
@@ -1,8 +1,9 @@
 Program maespa_growth
 Use Initialize, only: maespa_initialize, maespa_finalize
-USE maindeclarations ! This contains all variables that were defined in the program unit of maespa.
+USE maindeclarations, only: istart, iday, iend, mflag, nstep, RYTABLE1, RXTABLE1, RZTABLE1, FOLTABLE1, ZBCTABLE1, totRespf, totCO2 ! This contains all variables that were defined in the program unit of maespa.
 Use growth_module ! This containts the parameter and state variables and the growth module
 Implicit None
+Double precision :: Assimilation
 
 ! This reads all the input files and initializes all arrays. It corresponds to the all the code that appear before the daily loop in maespa
 call maespa_initialize
@@ -14,20 +15,27 @@ IDAY = 0
 DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
 
 ! Override the arrays with dimensions of all the trees in the orchard
-    RYTABLE1(1,:) = 0.37050426492714 ! Radius of green crown in y direction
-    RXTABLE1(1,:) = 0.37050426492714 ! Radius of green crown in x direction
-    RZTABLE1(1,:) = 0.852159809332423 ! Diameter of green crown in z direction   
-    FOLTABLE1(1,:) = 0.49 ! Total leaf area of a crown
-    ZBCTABLE1(1,:) = 0.5 ! Trunk height (i.e. from groun to first green branch)
-
+    RYTABLE1(1,:) = Rx ! Radius of green crown in y direction
+    RXTABLE1(1,:) = Ry ! Radius of green crown in x direction
+    RZTABLE1(1,:) = H ! Diameter of green crown in z direction   
+    FOLTABLE1(1,:) = LAD*Volume ! Total leaf area of a crown.
+    ZBCTABLE1(1,:) = max(H/2.0,0.5) ! Trunk height (i.e. from groun to first green branch)
 ! Call the main code of maespa. This corresponds to all the code that was located within the daily loop of maespa
     call run_maespa
 
-! Just to check: retrieve daily variables and print them
-    write(*,*) "Daily gross photos is", totCO2(1) + totrespf(1)
-    write(*,*) "Daily respiration is", totrespf(1)
-    write(*,*) "Daily absorbed PAR is", tdyab(1,1)
-
+! Update biomass
+    Assimilation = (totCO2(1) + totRespf(1))/d_alley/d_row*12.0
+! Update the state variables    
+    Biomass_leaf = Biomass_leaf + Assimilation*PC_leaf*PV_leaf/d_alley/d_row    
+    Volume       = Volume       + Assimilation*PC_leaf*PV_leaf*specific_leaf_area/LAD
+    LAI          = LAI          + Assimilation*PC_leaf*PV_leaf/d_alley/d_row *specific_leaf_area
+    If(trim(DensOpt) == trim('H')) THEN
+      Rx = min((0.125*Volume/cp)**(1.0/3.0), d_alley/3.0)
+      Ry = min((0.125*Volume/cp)**(1.0/3.0), d_row/2.0)
+      H  = Volume/4.0/Rx/Ry
+    End if
+! Write something to the output
+    Call  write_growth_outputs
 ! Proceed to the next step. Borrowed from the original daily loop of maespa
     IDAY = IDAY + NSTEP
     IF ((ISTART+IDAY) <= IEND) THEN
@@ -35,11 +43,6 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     END IF
         
 END DO ! End daily loop
-
-write(*,*) OptPrun
-write(*,*) DOYPhen2
-write(*,*) RmRef_stem, Biomass_leaf
-write(*,*) DensOpt
 
 ! This closes files and does other finalization operations. It corresponds to the all the code that appear after the daily loop in maespa
 call maespa_finalize
