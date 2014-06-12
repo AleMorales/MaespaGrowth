053fd29d331f2d8a7f5f8bee46ee3612aa0dd5ab
Alejandro Morales <morales.s.alejandro@gmail.com>
2014-06-12 09:41:55 +0200

    Add declarations for cohorts fo shoots

--- growth_module.f90
+++ growth_module.f90
@@ -1,13 +1,13 @@
 Module growth_module
 Implicit None
 INTEGER :: IOERROR
-double precision :: RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, RF_leaf, RF_shoots, RF_stem, RF_froots, RF_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, CC_leaf, CC_shoots, CC_stem, CC_froots, CC_croots, LAD, Q10, cp,Hmax, Kroots, CCfr, CCoil, RFfr, RFoil, PGoil, PGfr, Kf, PVfr, PVoil, FractionReproductive, OneLeaf,  PVres, CCres, PCfr, PCoil, d_alley, d_row, Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Volume, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, specific_leaf_area, RmRef_reserves, ColdRequirement, ThermalTimeRequirement, Phen_T0, Phen_Tx, Phen_a, Phen_Tb, ChillingHours, ThermalTime, ActiveWood
+double precision :: RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, RF_leaf, RF_shoots, RF_stem, RF_froots, RF_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, CC_leaf, CC_shoots, CC_stem, CC_froots, CC_croots, LAD, Q10, cp,Hmax, Kroots, CCfr, CCoil, RFfr, RFoil, PGoil, PGfr, Kf, PVfr, PVoil, FractionReproductive, OneLeaf,  PVres, CCres, PCfr, PCoil, d_alley, d_row, Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Volume, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, specific_leaf_area, RmRef_reserves, ColdRequirement, ThermalTimeRequirement, Phen_T0, Phen_Tx, Phen_a, Phen_Tb, ChillingHours, ThermalTime, ActiveWood, Biomass_shoots0, Biomass_shoots1, Biomass_shoots2, Biomass_shoots2T
 integer :: OptPrun, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, Adult, DOYsenescence1, DOYsenescence2, Age
 character(LEN = 10) :: DensOpt, OptFruit
 
 Namelist /growth_parameters/ RmRef_leaf, RmRef_shoots, RmRef_stem, RmRef_froots, RmRef_croots, RmRef_fruits, PC_leaf, PC_shoots, PC_stem, PC_froots, PC_croots, PV_leaf, PV_shoots, PV_stem, PV_froots, PV_croots, LAD, Q10, cp, Hmax, DensOpt, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, Kroots, PVfr, PVoil, Adult, DOYsenescence1, DOYsenescence2, PVres, CCres, PCfr, PCoil, OptPrun, d_alley, d_row, specific_leaf_area, RmRef_reserves, ThermalTimeRequirement, ColdRequirement, Phen_T0, Phen_Tx, Phen_a, Phen_Tb, ActiveWood
 
-Namelist /growth_initial/ Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Volume, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, Age, ChillingHours, ThermalTime
+Namelist /growth_initial/ Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Volume, LAI, H, Rx, Ry, Reserves, ReservesT, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, Biomass_leaf2T, Biomass_shoots0, Biomass_shoots1, Biomass_shoots2, Biomass_shoots2T, Age, ChillingHours, ThermalTime
 
 contains
 
@@ -16,7 +16,7 @@ subroutine read_growth_inputs
 USE maestcom, only: ugrowth, out_growth, ifatal
 Implicit none
 
-! Open the input file called 'Growth.dat'   
+! Open the input file called 'Growth.dat'
     OPEN(UGROWTH, FILE= 'Growth.dat', STATUS = 'OLD', IOSTAT = IOERROR)
 ! Trigger a warning if the file does not exist (model runs with default values)
     IF(IOERROR.NE.0) THEN
@@ -29,23 +29,23 @@ Implicit none
     IF(IOERROR.NE.0) THEN
         close(ugrowth)
         CALL SUBERROR('ERROR: namelist growth_parameters was not red properly', IFATAL,IOERROR)
-    ENDIF      
-    rewind(UGROWTH) 
+    ENDIF
+    rewind(UGROWTH)
 ! Read the initial values for the state and auxilliary variables
-	Read(ugrowth, nml = growth_initial, IOSTAT = IOERROR)    
+	Read(ugrowth, nml = growth_initial, IOSTAT = IOERROR)
 ! Trigger a warning if there is a problem reading the namelist
     IF(IOERROR.NE.0) THEN
         close(ugrowth)
         CALL SUBERROR('ERROR: namelist growth_initial was not red properly', IFATAL,IOERROR)
-    ENDIF    
+    ENDIF
     close(ugrowth)
 
-! Open the output file called 'out_growth.dat'   
+! Open the output file called 'out_growth.dat'
     OPEN(out_growth, FILE= 'out_growth.dat', STATUS = 'OLD', IOSTAT = IOERROR,ACTION = 'WRITE')
     IF(IOERROR.NE.0) THEN
     	close(out_growth)
         CALL SUBERROR('ERROR: out_growth.dat does not exist', IFATAL,IOERROR)
-    ENDIF 
+    ENDIF
     Write(out_growth, '(22(A14,3x))') 'Year','DOY','Leaf','Shoots','Stem','Fine_roots','Coarse_roots','Fruits','Reserves','Volume','LAI','Height','Radius_x','Radius_y', 'Assimilation','M.Respiration','Leaf_0','Leaf_1','Leaf_2','RUEab','aPAR','PAR'
 end subroutine read_growth_inputs
 
@@ -56,7 +56,7 @@ integer, intent(in) :: Year, DOY
 double precision, intent(in) :: RUE, Assimilation, RmD, aPAR
 real, intent(in) :: PAR
 	Write(out_growth, '(I2,3x,I3,2x,20(ES16.6E3,3x))') Year, DOY, Biomass_leaf, Biomass_shoots, Biomass_stem, Biomass_froots, Biomass_croots, Biomass_fruits, Reserves, Volume, LAI, H, Rx, Ry, Assimilation, RmD, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, RUE, aPAR, PAR
-end subroutine 
+end subroutine
 
 subroutine growth_finalize
 Use maestcom, only: out_growth
@@ -65,4 +65,3 @@ Implicit None
 end subroutine growth_finalize
 
 End module growth_module
-
