7e27f0c3646bac6c3e9519c5407e3ac9f45fb9a9
Alejandro Morales <morales.s.alejandro@gmail.com>
2014-06-12 09:44:59 +0200

    Add shoot cohorts to the update algorithm at the beginning of each year

--- maespa_growth.f90
+++ maespa_growth.f90
@@ -23,13 +23,17 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     DOY = mod(iday, 365) + 1
     Year = iday/365 + 1
 
-    ! Update age and leaf cohorts
-    If(DOY == 1) Then 
+    ! Update age, leaf cohorts and shoot cohorts
+    If(DOY == 1) Then
         Age = Age + 1
         biomass_leaf2 = biomass_leaf1
         biomass_leaf2T = biomass_leaf2
         biomass_leaf1 = biomass_leaf0
         biomass_leaf0 = 0.0
+        biomass_shoots2 = biomass_shoots1
+        biomass_shoots2T = biomass_shoots2
+        biomass_shoots1 = biomass_shoots0
+        biomass_shoots0 = 0.0        
     End if
 
     ! Turn on senescence in the case when senescence concentrates on a period of the year
@@ -45,7 +49,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
         ThermalTime = 0.0
     end if
 
-    ! Phenological stages plus harvest/pruning. 
+    ! Phenological stages plus harvest/pruning.
     ! Note that thermaltime and chilling hours are calculated after this code, so they refer to the previous day of simulation
     ! One could think of this code happening at the beginning of each day and temperature-related and carbon allocation happening at the end of the day
     ! It also means that harvesting and pruning is done "at the beginning of the day"
@@ -69,53 +73,53 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
             ! Pruning for high density orchards is always applied but differs when H > Hmax or H <= Hmax
             if(trim(DensOpt) == 'H') Then
             ! Prunning may want to control maximum height
-                if(H > Hmax) Then                                                      
-                    H = Hmax  
+                if(H > Hmax) Then
+                    H = Hmax
                     Volume = Hmax*Rx*Ry*4.0/3.0/2.0*pi
                     Rx = min((0.75*Volume/cp/pi)**(1.0/3.0), d_alley/3.0)
-                    Ry = min((0.75*Volume/cp/pi)**(1.0/3.0), d_row/2.0)                                                   
-                    Volume = Hmax*Rx*Ry*4.0/3.0/2.0*pi ! Because Rx and Ry may be limited by tree spacing  
+                    Ry = min((0.75*Volume/cp/pi)**(1.0/3.0), d_row/2.0)
+                    Volume = Hmax*Rx*Ry*4.0/3.0/2.0*pi ! Because Rx and Ry may be limited by tree spacing
                 end if
-            ! Calculate LADv and prune LAD if bigger. Use empirical rule from Orgaz et al. (2007)    
+            ! Calculate LADv and prune LAD if bigger. Use empirical rule from Orgaz et al. (2007)
                 if(Volume/(D_row*D_alley) < 0.5) then
                     LADv = 2.0
                 else
                     LADv = 2.0 - 0.8*(Volume/(d_row*d_alley) - 0.5)/1.5
-                end if      
-                if(LAD > LADv) LAD = LADv              
-            ! Update all state variables and canopy dimensions 
-                LAI = Volume/(d_alley*d_row)*LAD  
-                cohort0 = biomass_leaf0/biomass_leaf                
+                end if
+                if(LAD > LADv) LAD = LADv
+            ! Update all state variables and canopy dimensions
+                LAI = Volume/(d_alley*d_row)*LAD
+                cohort0 = biomass_leaf0/biomass_leaf
                 cohort1 = biomass_leaf1/biomass_leaf
-                cohort2 = biomass_leaf2/biomass_leaf                
-                ratio_leaf_shoots =    Biomass_leaf/Biomass_shoots   
-                ratio_leaf_stem   =    Biomass_leaf/Biomass_stem 
-                Biomass_leaf = LAI/specific_leaf_area  
-                Biomass_leaf0 = cohort0*Biomass_leaf  
-                Biomass_leaf1 = cohort1*Biomass_leaf  
-                Biomass_leaf2 = cohort2*Biomass_leaf                                                                             
+                cohort2 = biomass_leaf2/biomass_leaf
+                ratio_leaf_shoots =    Biomass_leaf/Biomass_shoots
+                ratio_leaf_stem   =    Biomass_leaf/Biomass_stem
+                Biomass_leaf = LAI/specific_leaf_area
+                Biomass_leaf0 = cohort0*Biomass_leaf
+                Biomass_leaf1 = cohort1*Biomass_leaf
+                Biomass_leaf2 = cohort2*Biomass_leaf
                 Biomass_shoots = Biomass_leaf/ratio_leaf_shoots
-                Biomass_stem = Biomass_leaf/ratio_leaf_stem      
-            ! Prunning for super-high density orchards only applied when H > Hmax   
+                Biomass_stem = Biomass_leaf/ratio_leaf_stem
+            ! Prunning for super-high density orchards only applied when H > Hmax
             else if (trim(DensOpt) == 'SH' .AND. H > Hmax) Then
-            ! Update all state variables and canopy dimensions    
-                H = Hmax  
+            ! Update all state variables and canopy dimensions
+                H = Hmax
                 Volume = Hmax*4.0*Rx*Ry
                 Rx = min((0.125*Volume/cp)**(1.0/3.0), d_alley/3.0)
-                Ry = min((0.125*Volume/cp)**(1.0/3.0), d_row/2.0)                                                       
-                Volume = Hmax*4.0*Rx*Ry ! Because Rx and Ry may be limited by tree spacing                                                
-                LAI = Volume*LAD/d_alley/d_row 
-                cohort0 = biomass_leaf0/biomass_leaf                
+                Ry = min((0.125*Volume/cp)**(1.0/3.0), d_row/2.0)
+                Volume = Hmax*4.0*Rx*Ry ! Because Rx and Ry may be limited by tree spacing
+                LAI = Volume*LAD/d_alley/d_row
+                cohort0 = biomass_leaf0/biomass_leaf
                 cohort1 = biomass_leaf1/biomass_leaf
-                cohort2 = biomass_leaf2/biomass_leaf                 
-                ratio_leaf_shoots =    Biomass_leaf/Biomass_shoots   
-                ratio_leaf_stem   =    Biomass_leaf/Biomass_stem                                  
-                Biomass_leaf = LAI/specific_leaf_area        
-                Biomass_leaf0 = cohort0*Biomass_leaf  
-                Biomass_leaf1 = cohort1*Biomass_leaf  
-                Biomass_leaf2 = cohort2*Biomass_leaf                                                          
+                cohort2 = biomass_leaf2/biomass_leaf
+                ratio_leaf_shoots =    Biomass_leaf/Biomass_shoots
+                ratio_leaf_stem   =    Biomass_leaf/Biomass_stem
+                Biomass_leaf = LAI/specific_leaf_area
+                Biomass_leaf0 = cohort0*Biomass_leaf
+                Biomass_leaf1 = cohort1*Biomass_leaf
+                Biomass_leaf2 = cohort2*Biomass_leaf
                 Biomass_shoots = Biomass_leaf/ratio_leaf_shoots
-                Biomass_stem = Biomass_leaf/ratio_leaf_stem     
+                Biomass_stem = Biomass_leaf/ratio_leaf_stem
             end if
         end if
     else if (DOY .GT. DOYPhen4) then
@@ -125,7 +129,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
 ! Override the arrays with dimensions of all the trees in the orchard
     RYTABLE1(1,:) = Ry ! Radius of green crown in y direction
     RXTABLE1(1,:) = Rx ! Radius of green crown in x direction
-    RZTABLE1(1,:) = H ! Diameter of green crown in z direction   
+    RZTABLE1(1,:) = H ! Diameter of green crown in z direction
     FOLTABLE1(1,:) = LAD*Volume ! Total leaf area of a crown.
     ZBCTABLE1(1,:) = min(H/2.0,0.5) ! Trunk height (i.e. from ground to first green branch). Not really much effect on simulation (unless we want info on the ground)
 
@@ -172,13 +176,13 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
             Allocation_froots = PC_froots
             Allocation_croots = PC_croots
             Allocation_reserves = 0.0
-            reallocation        = 1.0            
+            reallocation        = 1.0
         Case (3)
-            if(Age .GE. Adult) Then        
+            if(Age .GE. Adult) Then
                 Allocation_fruits  = PCfr
             else
                 Allocation_fruits = 0.0
-            End if             
+            End if
             Allocation_leaf = PC_leaf*(1.0 - Allocation_fruits)
             Allocation_shoots = PC_shoots*(1.0 - Allocation_fruits)
             Allocation_stem = PC_stem*(1.0 - Allocation_fruits)
@@ -186,7 +190,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
             Allocation_croots = PC_croots*(1.0 - Allocation_fruits)
             Allocation_reserves = 0.0
             PV_fruits = PVfr
-            reallocation        = 0.0            
+            reallocation        = 0.0
         Case (4)
             if(Age .GE. Adult) Then
                 Allocation_fruits = PCoil
@@ -200,21 +204,21 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
             Allocation_croots = PC_croots*(1.0 - Allocation_fruits)
             Allocation_reserves = 0.0
             PV_fruits           = PVoil
-            reallocation        = 0.0            
+            reallocation        = 0.0
     End Select
 
 ! Pool of assimilates produce on a given day (g C (m2 ground)-2)
     Assimilation = (totCO2(1) + totRespf(1))/d_alley/d_row*12.0
 ! Maintenance respiration. ! Average maintenance respiration on a daily basis (g C (m2 ground)-2)
-    RmD          = sum((Biomass_leaf*RmRef_leaf + Biomass_shoots*RmRef_shoots + Biomass_stem*RmRef_stem*ActiveWood + Biomass_froots*RmRef_froots + Biomass_croots*RmRef_croots*ActiveWood + Biomass_fruits*RmRef_fruits + Reserves*RmRef_reserves)*Q10**((TAIR(1:KHRS) - 25.0)/10.0))*24.0/KHRS 
-! Source limitation of photosynthesis 
+    RmD          = sum((Biomass_leaf*RmRef_leaf + Biomass_shoots*RmRef_shoots + Biomass_stem*RmRef_stem*ActiveWood + Biomass_froots*RmRef_froots + Biomass_croots*RmRef_croots*ActiveWood + Biomass_fruits*RmRef_fruits + Reserves*RmRef_reserves)*Q10**((TAIR(1:KHRS) - 25.0)/10.0))*24.0/KHRS
+! Source limitation of photosynthesis
     If(RmD > Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres) RmD = Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres
 ! Pool of assimilates (g C (m ground)-2)
     Pool         = Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres - RmD
-! Update the state variables 
+! Update the state variables
 ! Leaf biomass (g dm (m ground)-2)
     Biomass_leaf = Biomass_leaf + Pool*Allocation_leaf*PV_leaf - senescence*biomass_leaf2T/(DOYsenescence2 - DOYsenescence1)
-    biomass_leaf0  = biomass_leaf0 + Pool*Allocation_leaf*PV_leaf 
+    biomass_leaf0  = biomass_leaf0 + Pool*Allocation_leaf*PV_leaf
     biomass_leaf2 = biomass_leaf2 - senescence*biomass_leaf2T/(DOYsenescence2 - DOYsenescence1)
 ! Volume of an individual crown (m3)
     Volume       = Volume       + (Pool*Allocation_leaf*PV_leaf - senescence*biomass_leaf2T/(DOYsenescence2 - DOYsenescence1))*specific_leaf_area/LAD*d_alley*d_row
@@ -225,7 +229,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
       Rx = min((0.125*Volume/cp)**(1.0/3.0), d_alley/3.0)
       Ry = min((0.125*Volume/cp)**(1.0/3.0), d_row/2.0)
       H  = Volume/4.0/Rx/Ry
-    else if(trim(DensOpt) == 'H') Then      
+    else if(trim(DensOpt) == 'H') Then
      Rx  = min((0.75*Volume/cp/pi)**(1.0/3.0), d_alley/3.0)
      Ry  = min((0.75*Volume/cp/pi)**(1.0/3.0), d_row/2.0)
      H   = 0.75*Volume/pi/Rx/Ry*2.0
@@ -249,7 +253,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     IF ((ISTART+IDAY) <= IEND) THEN
         CALL SKIPMET(MFLAG,NSTEP)
     END IF
-        
+
 END DO ! End daily loop
 
 ! This closes files and does other finalization operations. It corresponds to the all the code that appear after the daily loop in maespa
@@ -257,4 +261,3 @@ call maespa_finalize
 call growth_finalize
 
 End Program maespa_growth
-
