b6be3b0973e7e15408ce4df12a7b6c36bbbe35b7
AleMorales <morales.s.alejandro@gmail.com>
2015-05-24 11:41:41 +0200

    Move calculation of PC and phenology to subroutine. Reorganize code in main program concerning reserves management (as it depends on the phenological model).

--- maespa_growth.f90
+++ maespa_growth.f90
@@ -24,7 +24,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     DOY = mod(iday, 365) + 1
     Year = iday/365 + 1
 
-    ! Update age and leaf cohorts
+    ! Update age and leaf cohorts at the beginning of each simulation year
     If(DOY == 1) Then
         Age = Age + 1
         biomass_leaf2 = biomass_leaf1
@@ -33,7 +33,7 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
         biomass_leaf0 = 0.0
     End if
 
-    ! Turn on senescence in the case when senescence concentrates on a period of the year
+    ! Turn on senescence within the period for leaf senescence
     if(DOY .ge. DOYsenescence1 .AND. DOY .LT. DOYsenescence2) Then
         senescence = 1.0
     else
@@ -41,40 +41,15 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     end if
 
 
-    ! Phenological stages plus harvest/pruning.
-    ! Note that thermaltime and chilling hours are calculated after this code, so they refer to the previous day of simulation
-    ! One could think of this code happening at the beginning of each day and temperature-related and carbon allocation happening at the end of the day
-    ! It also means that harvesting and pruning is done "at the beginning of the day"
+    ! Calculate phenological stages with fixed dates
+    If (PhenSim == 0) Then
+      call Calc_Phen_Fixed(DOY, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, PhenStage, &
+                           OptPrun, DensOpt, H, Hmax, Rx, Ry, Volume, cp, D_row, D_alley, LAI, LAD, &
+                           Biomass_leaf, Biomass_stem, Biomass_froots, Biomass_fruits, specific_leaf_area, &
+                           ActiveWood, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2)
+    ! Or based on thermal time and accumulation og chilling hours
+    End If
 
-    !
-    if(DOY .LT. DOYPhen1) Then
-        PhenStage = 1
-        residues = 0
-    else if(DOY .LT. DOYPhen2) Then
-    !else if((ChillingHours < ColdRequirement .OR. ThermalTime < ThermalTimeRequirement) .AND. DOY < DOYPhen3) then
-        PhenStage = 2
-        residues = 0
-    else if(DOY .LT. DOYPhen3) Then
-    !else if(ChillingHours > ColdRequirement .AND. ThermalTime > ThermalTimeRequirement .AND. DOY < DOYPhen3) then
-        PhenStage = 3
-        residues = 0
-    else if(DOY .LT. DOYPhen4) Then
-    !else if(DOY .GE. DOYPhen3 .AND. DOY .LT. DOYPhen4) Then
-        PhenStage = 4
-        residues = 0
-    else if(DOY == DOYPhen4) Then
-        PhenStage = 1
-        ChillingHours = 0.0
-        ThermalTime = 0.0
-        Biomass_fruits = 0.0 ! Harvest
-        If (OptPrun == 1) Then
-          Call Pruning(DensOpt, H, Hmax, Rx, Ry, Volume, cp, D_row, D_alley, LAI, LAD, &
-                       Biomass_leaf, Biomass_stem, Biomass_froots, specific_leaf_area, &
-                       ActiveWood, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2)
-        End If
-    else if (DOY .GT. DOYPhen4) then
-        PhenStage = 1
-    end if
 
 ! Override the arrays with dimensions of all the trees in the orchard
     RYTABLE1(1,:) = Ry ! Radius of green crown in y direction
@@ -88,62 +63,38 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     call run_maespa
 
 ! When simulating phenology, calculate thermal time and chilling hours
+! This is done after calling Maespa because we use the weather data read by Maespa
   If(PhenSim == 1) Then
     call thermal_time(DayLength, TmaxDay, TminDay, 100, ColdRequirement, ChillingHours, &
                   Phen_T0, Phen_a, Phen_Tx, ThermalTimeRequirementFl, &
                   Phen_TbFl, ThermalTimeRequirementFr, Phen_TbFr, ThermalTime)
   End If
-! Carbon allocation
-    Select Case (PhenStage)
-        Case (1)
-            Allocation_fruits = 0.0
-            Allocation_leaf = 0.0
-            Allocation_stem = 0.0
-            Allocation_froots = 0.0
-            Allocation_croots = 0.0
-            Allocation_reserves = 1.0
-            reallocation        = 0.0
-        Case (2)
-            Allocation_fruits = 0.
-            Allocation_leaf = PC_leaf
-            Allocation_stem = PC_stem
-            Allocation_froots = PC_froots
-            Allocation_croots = PC_croots
-            Allocation_reserves = 0.0
-            reallocation        = 1.0
-        Case (3)
-            if(Age .GE. Adult) Then
-                Allocation_fruits  = PCfr
-            else
-                Allocation_fruits = 0.0
-            End if
-            Allocation_leaf = PC_leaf*(1.0 - Allocation_fruits)
-            Allocation_stem = PC_stem*(1.0 - Allocation_fruits)
-            Allocation_froots = PC_froots*(1.0 - Allocation_fruits)
-            Allocation_croots = PC_croots*(1.0 - Allocation_fruits)
-            Allocation_reserves = 0.0
-            PV_fruits = PVfr
-            reallocation        = 0.0
-        Case (4)
-            if(Age .GE. Adult) Then
-                Allocation_fruits = PCoil
-            else
-                Allocation_fruits = 0.0
-            End if
-            Allocation_leaf = PC_leaf*(1.0 - Allocation_fruits)
-            Allocation_stem = PC_stem*(1.0 - Allocation_fruits)
-            Allocation_froots = PC_froots*(1.0 - Allocation_fruits)
-            Allocation_croots = PC_croots*(1.0 - Allocation_fruits)
-            Allocation_reserves = 0.0
-            PV_fruits           = PVoil
-            reallocation        = 0.0
-    End Select
+
+! Carbon allocation when using fixed phenology
+  If(PhenSim == 0) Then
+    Call Calc_PC(PhenStage, Allocation_fruits, Allocation_leaf, Allocation_stem, &
+                       Allocation_froots, Allocation_croots, Allocation_reserves, &
+                       reallocation, PC_leaf, PC_stem, PC_froots, PC_croots, PCfr, PCoil, &
+                       PV_fruits, PVfr, PVoil)
+  End If
 
 ! Pool of assimilates produce on a given day (g C (m2 ground)-2)
     Assimilation = (totCO2(1) + totRespf(1))/d_alley/d_row*12.0
 
+! Management of reserves and creation of daily pool (g C (m ground)-2)
+! Reserve management depends on how we calculate phenology
+  if(PhenSim == 0 .AND. PhenStage == 1) ReservesT = Reserves
+  Reserves = Reserves + Pool*Allocation_reserves*PVres
+  If(PhenSim == 0 .AND. DOY >= DOYPhen1 .AND. DOY <=  DOYPhen2) Then
+    Pool = Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres
+    Reserves = Reserves - reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres
+  Else
+    Pool = Assimilation + Reserves*Kreallocation
+    Reserves = Reserves(1.0 - Kreallocation)
+  End If
+
 ! Maintenance respiration. ! Average maintenance respiration on a daily basis (g C (m2 ground)-2)
-    RmD          = sum((Biomass_leaf*RmRef_leaf + min(Biomass_stem, Biomass_leaf*ActiveWood)*RmRef_stem + Biomass_froots*RmRef_froots + min(Biomass_croots, Biomass_froots*ActiveWood)*RmRef_croots + Biomass_fruits*RmRef_fruits + Reserves*RmRef_reserves)*Q10**((TAIR(1:KHRS) - 25.0)/10.0))*24.0/KHRS
+    RmD = sum((Biomass_leaf*RmRef_leaf + min(Biomass_stem, Biomass_leaf*ActiveWood)*RmRef_stem + Biomass_froots*RmRef_froots + min(Biomass_croots, Biomass_froots*ActiveWood)*RmRef_croots + Biomass_fruits*RmRef_fruits + Reserves*RmRef_reserves)*Q10**((TAIR(1:KHRS) - 25.0)/10.0))*24.0/KHRS
 
 ! Optionally, limit the maximum daily rate of photosynthesis
     If (WinterOpt == 1 .AND. PhenStage == 1 .AND. Assimilation > RmD*MaxPhotos) Then
@@ -151,15 +102,14 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     end if
 
 ! Source-limited maintenance respiration
-    If(RmD > Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres) then
-      RmD = Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres
-! Pool of assimilates (g C (m ground)-2)
-      Pool      = 0.0
+    If(RmD > Pool) then
+      RmD = Pool
+      Pool = 0.0
     else
-      Pool = Assimilation + reallocation*ReservesT/(DOYPhen2 - DOYPhen1)*CCres - RmD
+      Pool = Pool - RmD
     end if
-! Update the state variables
 
+! Update the state variables
 ! Fruit biomass (g dm (m ground)-2)
 ! It may optionally be sink-limited (FruitOpt == 1) which alters the allocation coefficients (the rest will increase)
     if(FruitOpt == 1 .AND. Pool*Allocation_fruits*PV_fruits > FruitMax) then
@@ -172,14 +122,18 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
     else
       Biomass_fruits = Biomass_fruits + Pool*Allocation_fruits*PV_fruits
     end if
+
 ! Leaf biomass (g dm (m ground)-2)
     Biomass_leaf = Biomass_leaf + Pool*Allocation_leaf*PV_leaf - senescence*biomass_leaf2T/(DOYsenescence2 - DOYsenescence1)
     biomass_leaf0  = biomass_leaf0 + Pool*Allocation_leaf*PV_leaf
     biomass_leaf2 = biomass_leaf2 - senescence*biomass_leaf2T/(DOYsenescence2 - DOYsenescence1)
+
 ! Volume of an individual crown (m3)
     Volume       = Volume       + (Pool*Allocation_leaf*PV_leaf - senescence*biomass_leaf2T/(DOYsenescence2 - DOYsenescence1))*specific_leaf_area/LAD*d_alley*d_row
+
 ! Leaf area index (m2 leaf (m ground)-2)
     LAI          = Volume/d_alley/d_row*LAD
+
 ! Radius x and y(m). Diameter z (m)
     If(trim(DensOpt) == trim('SH')) THEN
       Rx = min((0.125*Volume/cp)**(1.0/3.0), d_alley/3.0)
@@ -190,13 +144,12 @@ DO WHILE (ISTART + IDAY <= IEND) ! start daily loop
      Ry  = min((0.75*Volume/cp/pi)**(1.0/3.0), d_row/2.0)
      H   = 0.75*Volume/pi/Rx/Ry*2.0
     End if
+
 ! Biomass of stem, froots, croots, fruits and reserves (g C (m ground)-2)
     Biomass_stem   = Biomass_stem   + Pool*Allocation_stem*PV_stem
     Biomass_froots = Biomass_froots + Pool*Allocation_froots*PV_froots - Biomass_froots*Kroots
     root_loss      = Biomass_froots*Kroots
     Biomass_croots = Biomass_croots + Pool*Allocation_croots*PV_croots
-    Reserves       = Reserves       + Pool*Allocation_reserves*PVres  - reallocation*ReservesT/(DOYPhen2 - DOYPhen1)
-    if(PhenStage == 1) ReservesT = Reserves
 
 ! Write state variables and fluxes to the output
     Call  write_growth_outputs(Year, DOY, Assimilation, RmD, TDYAB(1,1)/d_alley/d_row, sum(RADABV(1:KHRS, 1))*SPERHR/1e6, residues, root_loss, (TminDay + TmaxDay)/2.0)
@@ -349,3 +302,102 @@ subroutine pruning(DensOpt, H, Hmax, Rx, Ry, Volume, cp, D_row, D_alley, LAI, LA
       Biomass_froots = Biomass_leaf/ratio_leaf_froots
   end if
 end subroutine pruning
+
+
+subroutine Calc_PC(PhenStage, Allocation_fruits, Allocation_leaf, Allocation_stem, &
+                   Allocation_froots, Allocation_croots, Allocation_reserves, &
+                   reallocation, PC_leaf, PC_stem, PC_froots, PC_croots, PCfr, PCoil, &
+                   PV_fruits, PVfr, PVoil)
+  ! Arguments
+  integer, intent(in) :: PhenStage
+  double precision, intent(in) :: PC_leaf, PC_stem, PC_froots, PC_croots, PCfr, PCoil, &
+                                  PVfr, PVoil
+  double precision, intent(out) :: Allocation_fruits, Allocation_leaf, Allocation_stem, &
+                                   Allocation_froots, Allocation_croots, Allocation_reserves, &
+                                   reallocation, PV_fruits
+  Select Case (PhenStage)
+      Case (1)
+          Allocation_fruits = 0.0
+          Allocation_leaf = 0.0
+          Allocation_stem = 0.0
+          Allocation_froots = 0.0
+          Allocation_croots = 0.0
+          Allocation_reserves = 1.0
+          reallocation        = 0.0
+      Case (2)
+          Allocation_fruits = 0.
+          Allocation_leaf = PC_leaf
+          Allocation_stem = PC_stem
+          Allocation_froots = PC_froots
+          Allocation_croots = PC_croots
+          Allocation_reserves = 0.0
+          reallocation        = 1.0
+      Case (3)
+          if(Age .GE. Adult) Then
+              Allocation_fruits  = PCfr
+          else
+              Allocation_fruits = 0.0
+          End if
+          Allocation_leaf = PC_leaf*(1.0 - Allocation_fruits)
+          Allocation_stem = PC_stem*(1.0 - Allocation_fruits)
+          Allocation_froots = PC_froots*(1.0 - Allocation_fruits)
+          Allocation_croots = PC_croots*(1.0 - Allocation_fruits)
+          Allocation_reserves = 0.0
+          PV_fruits = PVfr
+          reallocation        = 0.0
+      Case (4)
+          if(Age .GE. Adult) Then
+              Allocation_fruits = PCoil
+          else
+              Allocation_fruits = 0.0
+          End if
+          Allocation_leaf = PC_leaf*(1.0 - Allocation_fruits)
+          Allocation_stem = PC_stem*(1.0 - Allocation_fruits)
+          Allocation_froots = PC_froots*(1.0 - Allocation_fruits)
+          Allocation_croots = PC_croots*(1.0 - Allocation_fruits)
+          Allocation_reserves = 0.0
+          PV_fruits           = PVoil
+          reallocation        = 0.0
+  End Select
+
+end subroutine Calc_PC
+
+subroutine Calc_Phen_Fixed(DOY, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, PhenStage, &
+                          OptPrun, DensOpt, H, Hmax, Rx, Ry, Volume, cp, D_row, D_alley, LAI, LAD, &
+                           Biomass_leaf, Biomass_stem, Biomass_froots, Biomass_fruits, specific_leaf_area, &
+                           ActiveWood, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2)
+
+  ! Arguments
+  character(LEN = 10), intent(in) :: DensOpt
+  double precision, intent(in) :: D_row, D_alley, specific_leaf_area, ActiveWood, cp
+  integer, intent(in) :: DOY, DOYPhen1, DOYPhen2, DOYPhen3, DOYPhen4, OptPrun
+  double precision, intent(inout) :: Biomass_leaf, Biomass_stem, Biomass_froots, Biomass_fruits, &
+                                     Biomass_leaf0, Biomass_leaf1, Biomass_leaf2, &
+                                     H, Hmax, Rx, Ry, Volume, LAI, LAD
+  integer, intent(out) :: PhenStage
+
+
+  if(DOY .LT. DOYPhen1) Then
+      PhenStage = 1
+  else if(DOY .LT. DOYPhen2) Then
+  !else if((ChillingHours < ColdRequirement .OR. ThermalTime < ThermalTimeRequirement) .AND. DOY < DOYPhen3) then
+      PhenStage = 2
+  else if(DOY .LT. DOYPhen3) Then
+  !else if(ChillingHours > ColdRequirement .AND. ThermalTime > ThermalTimeRequirement .AND. DOY < DOYPhen3) then
+      PhenStage = 3
+  else if(DOY .LT. DOYPhen4) Then
+  !else if(DOY .GE. DOYPhen3 .AND. DOY .LT. DOYPhen4) Then
+      PhenStage = 4
+  else if(DOY == DOYPhen4) Then
+      PhenStage = 1
+      Biomass_fruits = 0.0 ! Harvest
+      If (OptPrun == 1) Then
+        Call Pruning(DensOpt, H, Hmax, Rx, Ry, Volume, cp, D_row, D_alley, LAI, LAD, &
+                     Biomass_leaf, Biomass_stem, Biomass_froots, specific_leaf_area, &
+                     ActiveWood, Biomass_leaf0, Biomass_leaf1, Biomass_leaf2)
+      End If
+  else if (DOY .GT. DOYPhen4) then
+      PhenStage = 1
+  end if
+
+end subroutine Calc_Phen_Fixed
